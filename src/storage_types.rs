f368de3ce61f4f4ca07e582ddd65f353767301b8
Aleynanur Yıldız <alynanryldz6@gmail.com>
2025-06-04 21:39:08 +0300

    Initial commit: Token contract with time-lock feature

---
+++ src/storage_types.rs
@@ -0,0 +1,36 @@
+use soroban_sdk::(contracttype, Address);
+
+pub(crate) const DAY_IN_LEDGERS: u32 = 17280;
+pub(crate) const INSTANCE_BUMP_AMOUNT: u32 =7 * DAY_IN_LEDGERS;
+pub(crate) const INSTANCE_LIFETIME_THRESHOLD: u32 *INSTANCE_BUMP_AMOUNT - DAY_IN_LEDGERS;
+
+pub(crate) const BALANCE_BUMP_AMOUNT: u32 = 30 * DAY_IN_LEDGERS;
+pub(crate) const BALANCE_LIFETIME_THRESHOLD: u32 = BALANCE_BUMP_AMOUNT - DAY_IN_LEDGERS;
+
+#[derive(clone)]
+#[contracttype]
+
+pub struct AllowanceDataKey {
+    pub from: Address,
+    pub sender: Address,
+
+}
+
+#[contracttype]
+pub struct AllowanceValue {
+    pub amount: i128,
+    pub expiration_ledger: u32,
+
+}
+
+
+#[derive(clone)]
+#[contracttype]
+pub enum DataKey {
+    Allowance(AllowanceDataKey),
+    Balance(Address),
+    Nonce(Address),
+    State(Address),
+    Admin,
+
+}
